11247c9c5f166501f81cc5699acca27b565bbc5d
Jacob William House <me@jwfh.ca>
2019-02-19 18:33:21 -0400

    Updated taskbar and sidebar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,50 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './assets/style/europa.scss';
 import './assets/style/bell.scss';
 import './App.scss';
-import RegisterForm from './components/RegisterForm';
 import TaskBar from './components/TaskBar';
-import SideBar from './components/SideBar'
+import SideBar from './components/SideBar';
+import HomePage from './components/HomePage';
+import LoginPage from './components/LoginPage';
+import RegisterForm from './components/RegisterForm';
+import ForgotPasswordPage from './components/ForgotPasswordPage';
+import Error404 from './components/Error404';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <TaskBar>
-
-        </TaskBar>
-        <RegisterForm 
-        
-        />
-        <SideBar>
-          
-        </SideBar>
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <TaskBar />
+          <Switch>
+            <Route 
+              path="/"
+              component={HomePage}
+              exact
+            />
+            <Route 
+              path="/register"
+              component={RegisterForm}
+              exact
+            />
+            <Route 
+              path="/login"
+              component={LoginPage}
+              exact
+            />
+            <Route
+              path="/forgot"
+              component={ForgotPasswordPage}
+              exact
+            />
+            <Route
+              component={Error404}
+            />
+          </Switch>
+          <SideBar />
+        </div>
+      </BrowserRouter>
     );
   }
 }
