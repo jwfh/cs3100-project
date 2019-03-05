54899506d9dc7a1d0b68ba5e4c6698ed9b9e4b59
Gillian <gnykm6@mun.ca>
2019-03-05 20:20:50 -0330

    Added login form

--- frontend/src/components/LoginForm.jsx
+++ frontend/src/components/LoginForm.jsx
@@ -1,18 +1,51 @@
 import React, { Component } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import Appbar from 'material-ui/AppBar';
+import RaisedButton from 'material-ui/RaisedButton';
+import TextField from 'material-ui/TextField';
+import LoginFormAccount from './LoginFormAccount';
+import LoginFormDetails from './LoginFormDetails';
+import { withStyles } from '@material-ui/core/styles';
+const styles = (theme) => ({
+  formBody: {
+    textAlign: 'center',
+  },
+  button: {
+    margin: 15,
+  },
+});
 
-export class LoginForm extends Component {
-  state = {
-    emailAddress: '',
+export class LoginForm extends Component { 
+  this.state = {
+    username: '',
     password: '',
-  };
-
-  render() {
-    return (
-      <div>
-        
-      </div>
-    );
   }
 }
-
+render() {
+  return (
+    <div>
+      <MuiThemeProvider>
+        <div>
+        <AppBar
+           title="Login"
+         />
+         <TextField
+           hintText="Enter your Username"
+           floatingLabelText="Username"
+           onChange = {(event,newValue) => this.setState({username:newValue})}
+           />
+         <br/>
+           <TextField
+             type="password"
+             hintText="Enter your Password"
+             floatingLabelText="Password"
+             onChange = {(event,newValue) => this.setState({password:newValue})}
+             />
+           <br/>
+           <RaisedButton label="Submit" primary={true} style={style} onClick={(event) => this.handleClick(event)}/>
+       </div>
+       </MuiThemeProvider>
+    </div>
+  );
+}
 export default LoginForm;
--- frontend/src/components/LoginPage.jsx
+++ frontend/src/components/LoginPage.jsx
@@ -1,13 +1,40 @@
 import React, { Component } from 'react';
-
-export class LoginPage extends Component {
-  render() {
-    return (
-      <div>
-        Login
-      </div>
-    );
-  }
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import RaisedButton from 'material-ui/RaisedButton';
+import LoginForm from './LoginForm';
+import RegisterForm from './RegisterForm';
+class LoginPage extends Component {
+    this.state={
+      username:'',
+      password:'',
+      loginpage:[],
+      loginmessage:'',
+      buttonLabel:'Register',
+      isLoginForm:true
+    }
+    componentWillMount(){;
+      var loginpage=[];
+      loginpage.push(<LoginForm parentContext={this} appContext={this.parentContext}/>);
+      var loginmessage = "Not registered yet, Register Now";
+      this.setState({
+        loginpage:loginpage,
+        loginmessage:loginmessage
+      })
+    }
+    render() {
+      return (
+        <div className="loginpage">
+          {this.state.loginpage}
+          <div>
+            {this.state.loginmessage}
+            <MuiThemeProvider>
+              <div>
+                <RaisedButton label={this.state.buttonLabel} primary={true} style={style} onClick={(event) => this.handleClick(event)}/>
+              </div>
+            </MuiThemeProvider>
+          </div>
+        </div>
+      );
+    }
 }
-
 export default LoginPage;
