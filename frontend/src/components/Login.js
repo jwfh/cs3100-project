e4e0f815f12dea0e881796600050421ba9ee3251
Gillian <gnykm6@mun.ca>
2019-03-13 20:02:57 -0230

    Login.js punctuation error fixed

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -16,68 +16,68 @@ const styles = (theme) => ({
     },
 });
 class Login extends Component {
-    constructor(props){
-      super(props);
-      this.state={
-      username:'',
-      password:''
-      }
+  constructor(props){
+    super(props);
+    this.state={
+    username:'',
+    password:''
     }
-    render() {
-        return (
+  }
+  render() {
+    return (
+      <div>
+        <MuiThemeProvider>
           <div>
-            <MuiThemeProvider>
-              <div>
-              <AppBar
-                 title="Login"
-               />
-               <TextField
-                 hintText="Enter your Username"
-                 floatingLabelText="Username"
-                 onChange = {(event,newValue) => this.setState({username:newValue})}
-                 />
-               <br/>
-                 <TextField
-                   type="password"
-                   hintText="Enter your Password"
-                   floatingLabelText="Password"
-                   onChange = {(event,newValue) => this.setState({password:newValue})}
-                   />
-                 <br/>
-                 <RaisedButton label="Submit" primary={true} style={styles} onClick={(event) => this.handleClick(event)}/>
-             </div>
-             </MuiThemeProvider>
+            <AppBar
+              title="Login"
+            />
+              <TextField
+              hintText="Enter your Username"
+              floatingLabelText="Username"
+              onChange = {(event,newValue) => this.setState({username:newValue})}
+              />
+              <br/>
+              <TextField
+                type="password"
+                hintText="Enter your Password"
+                floatingLabelText="Password"
+                onChange = {(event,newValue) => this.setState({password:newValue})}
+              />
+              <br/>
+              <RaisedButton label="Submit" primary={true} style={styles} onClick={(event) => this.handleClick(event)}/>
           </div>
-        )
+        </MuiThemeProvider>
+      </div>
+    )
+  }
+  handleClick(event){
+    var apiBaseUrl = "http://localhost:3000/api/";
+    var self = this;
+    var payload={
+    "email":this.state.username,
+    "password":this.state.password
     }
-    handleClick(event){
-        var apiBaseUrl = "http://localhost:3000/api/";
-        var self = this;
-        var payload={
-        "email":this.state.username,
-        "password":this.state.password
-        }
-        axios.post(apiBaseUrl+'login', payload)
-        .then(function (response) {
-        console.log(response);
-        if(response.data.code == 200){
-        console.log("Login successfull");
-        var uploadScreen=[];
-        uploadScreen.push(<UploadScreen appContext={self.props.appContext}/>)
-        self.props.appContext.setState({loginPage:[],uploadScreen:uploadScreen})
-        }
-        else if(response.data.code == 204){
-        console.log("Username password do not match");
-        alert("username password do not match")
-        }
-        else{
-        console.log("Username does not exists");
-        alert("Username does not exist");
-        }
-        })
-        .catch(function (error) {
-        console.log(error);
-        }
+    axios.post(apiBaseUrl+'login', payload)
+    .then(function (response) {
+    console.log(response);
+    if(response.data.code == 200){
+      console.log("Login successfull");
+      var uploadScreen=[];
+      uploadScreen.push(<UploadScreen appContext={self.props.appContext}/>)
+      self.props.appContext.setState({loginPage:[],uploadScreen:uploadScreen})
     }
+    else if(response.data.code == 204){
+      console.log("Username password do not match");
+      alert("username password do not match")
+    }
+    else{
+      console.log("Username does not exists");
+      alert("Username does not exist");
+    }
+    })
+    .catch(function (error) {
+    console.log(error);
+    });
+  }
 }
 export default withStyles(styles)(Login);
