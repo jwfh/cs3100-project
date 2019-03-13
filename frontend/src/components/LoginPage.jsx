ae1045359ded9c333452f05bc8ddce04eaade2f3
Gillian <gnykm6@mun.ca>
2019-03-13 13:46:54 -0230

    added the handleClick function to LoginForm and LoginPage
    HandleClick functions sends user details to backend on the click of the submit button

--- frontend/src/components/LoginPage.jsx
+++ frontend/src/components/LoginPage.jsx
@@ -37,4 +37,30 @@ class LoginPage extends Component {
       );
     }
 }
-export default withstyles(styles) (LoginPage);
+handleClick(event){
+  // console.log("event",event);
+  var loginmessage;
+  if(this.state.isLogin){
+    var loginscreen=[];
+    loginscreen.push(<Register parentContext={this}/>);
+    loginmessage = "Already registered.Go to Login";
+    this.setState({
+                   loginscreen:loginscreen,
+                   loginmessage:loginmessage,
+                   buttonLabel:"Login",
+                   isLogin:false
+                 })
+  }
+  else{
+    var loginscreen=[];
+    loginscreen.push(<Login parentContext={this}/>);
+    loginmessage = "Not Registered yet.Go to registration";
+    this.setState({
+                   loginscreen:loginscreen,
+                   loginmessage:loginmessage,
+                   buttonLabel:"Register",
+                   isLogin:true
+                 })
+  }
+}
+export default withStyles(styles) (LoginPage);
