4616ae699d7d82214b6505e99cbaa809cfa1014a
jwfh <me@jwfh.ca>
2019-03-18 18:29:27 -0230

    Prepared `login` for merge with `create`

--- frontend/src/components/LoginForm.jsx
+++ frontend/src/components/LoginForm.jsx
@@ -1,52 +1,67 @@
 import React, { Component } from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import Appbar from 'material-ui/AppBar';
 import RaisedButton from 'material-ui/RaisedButton';
 import TextField from 'material-ui/TextField';
 import { withStyles } from '@material-ui/core/styles';
+import { 
+  withRouter
+} from 'react-router-dom';
 
 const styles = (theme) => ({
-  container: {
-    textAlign: 'center',
-    marginTop: '15vh',
-  },
-  root: {
-    ...theme.mixins.gutters(),
-    paddingTop: theme.spacing.unit * 2,
-    paddingBottom: theme.spacing.unit * 2,
-    width: '75%',
-    display: 'inline-block',
-    textAlign: 'left',
-  },
+    formBody: {
+      textAlign: 'center',
+    },
+    button: {
+      margin: 15,
+    },
 });
 
-export class LoginForm extends Component { 
+const SubmitButton = withRouter(({ history, ...props }) => (
+  <RaisedButton  label="Submit" primary={true} onClick={() => {
+    props.handleError('test');
+    // history.push('/');
+  }}/>
+));
+
+class Login extends Component {
+
   state = {
     username: '',
     password: '',
-  }
+    errMsg: '',
+  };
+
+  // Handle fields change
+  handleChange = input => e => {
+    this.setState({
+      [input]: e.target.value,
+    });
+  };
+
   render() {
+    const { classes } = this.props;
     return (
+      <div>
         <MuiThemeProvider>
-          <React.Fragment>
-          <TextField
-            hintText="Enter your Username"
-            floatingLabelText="Username"
-            onChange = {(event,newValue) => this.setState({username:newValue})}
-            />
-          <br/>
+          <div>
             <TextField
-              type="password"
-              hintText="Enter your Password"
-              floatingLabelText="Password"
-              onChange = {(event,newValue) => this.setState({password:newValue})}
-              />
+              hintText="Enter your Username"
+              floatingLabelText="Username"
+              onChange={this.handleChange('username')}
+            />
             <br/>
-            <RaisedButton label="Submit" primary={true} onClick={(event) => this.handleClick(event)}/>
-            </React.Fragment>
+            <TextField
+                type="password"
+                hintText="Enter your Password"
+                floatingLabelText="Password"
+                onChange={this.handleChange('password')}
+            />
+            <br/> 
+            <SubmitButton handleError={(errMsg) => {this.setState( { errMsg: errMsg })}} />
+          </div>
         </MuiThemeProvider>
-    );
+      </div>
+    )
   }
 }
-
-export default LoginForm;
+export default withStyles(styles)(Login);
